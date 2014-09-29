a4992a1c5dd57761f605586369143cee7f24ff1e
DerjenigeWelche <masterfg@gmx.net>
2014-09-29 17:28:27 +0200

    Link

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/EntityFactory.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/EntityFactory.java
@@ -1,17 +1,20 @@
 package de.hochschuletrier.gdw.ss14.sandbox.Test;
 
+import de.hochschuletrier.gdw.ss14.sandbox.ecs.components.Component;
+
 public class EntityFactory {
 	
+	//http://vasir.net/blog/game-development/how-to-build-entity-component-system-in-javascript
 	public EntityFactory(){
 		
 	}
 	
-	public static void constructCat(){
-		
+	public static int constructCat(){
+		return 0;
 	}
 	
-	public static void constructDog(){
-		
+	public static int constructDog(){
+		return 0;
 	}
 	
 
