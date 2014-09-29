fb158fce503d8b2761f999c89158f40fb2397da6
DerjenigeWelche <masterfg@gmx.net>
2014-09-29 17:20:17 +0200

    FactoryGespeichert

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/EntityFactory.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/EntityFactory.java
@@ -1,5 +1,18 @@
 package de.hochschuletrier.gdw.ss14.sandbox.Test;
 
 public class EntityFactory {
+	
+	public EntityFactory(){
+		
+	}
+	
+	public static void constructCat(){
+		
+	}
+	
+	public static void constructDog(){
+		
+	}
+	
 
 }
