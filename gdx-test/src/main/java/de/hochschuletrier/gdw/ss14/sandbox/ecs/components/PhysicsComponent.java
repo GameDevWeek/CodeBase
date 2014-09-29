34c1c28d3b0e31e661f70665d9d035379cd1d71c
Daniel Dreher <blubbasd@googlemail.com>
2014-09-29 14:15:46 +0200

    Add basic Entity Component System.

---
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/ecs/components/PhysicsComponent.java
@@ -0,0 +1,17 @@
+package de.hochschuletrier.gdw.ss14.sandbox.ecs.components;
+
+import de.hochschuletrier.gdw.commons.gdx.physix.*;
+
+/**
+ * Created by Dani on 29.09.2014.
+ */
+public class PhysicsComponent extends PhysixEntity implements Component
+{
+    public PhysixBody physicsBody;
+
+    @Override
+    public void initPhysics(PhysixManager manager)
+    {
+
+    }
+}
