49abbc5d03c7204056e674b5b6dedc766057929a
DerjenigeWelche <masterfg@gmx.net>
2014-09-30 14:54:32 +0200

    Laufen Test

---
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/System/InputSystem.java
@@ -0,0 +1,25 @@
+package de.hochschuletrier.gdw.ss14.sandbox.Test.System;
+
+import de.hochschuletrier.gdw.ss14.sandbox.ecs.EntityManager;
+import de.hochschuletrier.gdw.ss14.sandbox.ecs.systems.ECSystem;
+
+public class InputSystem extends ECSystem{
+
+	public InputSystem(EntityManager entityManager) {
+		super(entityManager);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
