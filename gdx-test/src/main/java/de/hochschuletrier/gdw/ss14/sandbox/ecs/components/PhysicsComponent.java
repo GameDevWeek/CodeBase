1f41870cfdcc324dd952e04170a7a4d2e3149673
Daniel Dreher <blubbasd@googlemail.com>
2014-09-30 10:55:41 +0200

    Change ECS to properly divide update and render methods, add support for PhysicsComponents, add small examples.
    The ECS now places PhysicsComponents in the right component store using a class check before adding it.
    The examples show how to use the ECS with the Physix system and basic rendering.

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/ecs/components/PhysicsComponent.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/ecs/components/PhysicsComponent.java
@@ -5,6 +5,8 @@ import de.hochschuletrier.gdw.commons.gdx.physix.*;
 /**
  * Created by Dani on 29.09.2014.
  */
+
+// use this class as parent for other physicsComponents (e.g. CatPhysicsComponent, DogPhysicsComponent, ...)
 public class PhysicsComponent extends PhysixEntity implements Component
 {
     public PhysixBody physicsBody;
