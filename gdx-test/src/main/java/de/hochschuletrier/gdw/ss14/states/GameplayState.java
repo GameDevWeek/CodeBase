217f28f29caf5912cafa233d0574b38fd1461f2c
Djitin <djtinituz@googlemail.com>
2014-09-30 19:42:26 +0200

    Camera, TiledMapRenderer Systems work together in gdxTest.game

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/states/GameplayState.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/states/GameplayState.java
@@ -45,28 +45,29 @@ public class GameplayState extends GameState implements InputProcessor {
         Main.inputMultiplexer.addProcessor(this);
         
         // Setup camera
-        TiledMap map = game.getMap();
+        /*TiledMap map = game.getMap();
         camera.resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         totalMapWidth = map.getWidth() * map.getTileWidth();
         totalMapHeight = map.getHeight() * map.getTileHeight();
         camera.setBounds(0, 0, totalMapWidth, totalMapHeight);
         camera.updateForced();
-        Main.getInstance().addScreenListener(camera);
+        Main.getInstance().addScreenListener(camera);*/
     }
 
     @Override
     public void render() {
-        camera.bind();
+        /*camera.bind();
 
         DrawUtil.fillRect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), Color.BLACK);
 
         game.render();
-        DrawUtil.fillRect(position.x - 10, position.y -10, 20, 20, Color.RED);
+        DrawUtil.fillRect(position.x - 10, position.y -10, 20, 20, Color.RED);*/
+        game.render();
     }
 
     @Override
     public void update(float delta) {
-        emitter.update();
+        /*emitter.update();
         emitter.setPosition(cursor.x, cursor.y, 0);
         game.update(delta);
         camera.update(delta);
@@ -83,7 +84,9 @@ public class GameplayState extends GameState implements InputProcessor {
 
         position.x = Math.max(10, Math.min(totalMapWidth-10, position.x));
         position.y = Math.max(10, Math.min(totalMapHeight-10, position.y));
-        camera.setDestination(position);
+        camera.setDestination(position);*/
+        
+        game.update(delta);
         fpsCalc.addFrame();
     }
 
