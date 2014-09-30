03c128c85c921c39b961c71649abb99b86850f69
Rhabarberkuchen <michaela.moes@gmx.net>
2014-09-30 15:03:31 +0200

    StateComponent of animals added

---
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/CatStateComponent.java
@@ -0,0 +1,7 @@
+package de.hochschuletrier.gdw.ss14.sandbox.Test.Component;
+
+public class CatStateComponent {
+    public enum States {
+        DISTRACTED, WALKING, STANDING, RUNNING,
+    };
+}
---
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/DogStateComponent.java
@@ -0,0 +1,7 @@
+package de.hochschuletrier.gdw.ss14.sandbox.Test.Component;
+
+public class DogStateComponent {
+    public enum States {
+        RUNNING, STANDING, WALKING, KILLING
+    };
+}
