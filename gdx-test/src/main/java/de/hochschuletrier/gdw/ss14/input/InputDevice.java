0b25a694e764fd314582bc3630e028258e31ea83
Schirace <tm@christianmeier.de>
2014-09-30 14:00:33 +0200

    InputDevise - Methoden verschoben

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputDevice.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/input/InputDevice.java
@@ -1,5 +1,62 @@
 package de.hochschuletrier.gdw.ss14.input;
 
+import java.util.LinkedList;
+
+import de.hochschuletrier.gdw.ss14.sandbox.inputTest.GameInputAdapter;
+
 public abstract class InputDevice {
+
+	private LinkedList<GameInputAdapter> listener = new LinkedList<>(); 
+	
+    /**
+     * 
+     * @param gia @ GameInputAdapter to the Listener
+     */
+    public void addGameInputAdapter(GameInputAdapter gia) {
+    	listener.add(gia);
+    }
     
+    /**
+     * 
+     * @param gia remove GameInputAdapter gia from the listener
+     */
+    public void removeGameInputAdapter(GameInputAdapter gia) {
+    	listener.remove(gia);
+	}
+    
+    /**
+	 *  Laser on / off
+	 */
+	private void fireLaserButtonPressed() {	
+		for(GameInputAdapter inp: listener) {
+			inp.laserButtonPressed();
+		}
+	}
+	
+	/**
+	 * water pistol on
+	 */
+	private void fireWaterPistolButtonDown() {
+		for(GameInputAdapter inp: listener) {
+			inp.waterPistolButtonDown();
+		}
+	}
+	
+	/**
+	 * water pistol off
+	 */
+	private void fireWaterPistolButtonUp() {
+		for(GameInputAdapter inp: listener) {
+			inp.waterPistolButtonUp();
+		}
+	}
+	
+	/**
+	 * go to the menu or back
+	 */
+	private void fireMenuButtonPressed() {
+		for(GameInputAdapter inp: listener) {
+			inp.menueButtonPressed();
+		}
+	}
 }
