3495a54cbc24cb250ab8b7439e67d0a929359eb8
unknown <Janina.Noell@gmx.de>
2014-10-01 19:07:01 +0200

    ConePhysicsComponent modified(category and mask)

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/ConePhysicsComponent.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/Test/Component/ConePhysicsComponent.java
@@ -48,7 +48,7 @@ public class ConePhysicsComponent extends PhysicsComponent{
             startWinkel += delta;
         }
         
-        mShape.forEach((p)->System.out.println(p.x+" | "+p.y));
+        
     }
     
     public void initPhysics(PhysixManager manager){
@@ -59,8 +59,8 @@ public class ConePhysicsComponent extends PhysicsComponent{
                 .fixedRotation(true).angle(mRotation)
                 .create();
         
-    
-        physicsBody.createFixture(fixturedef.shapePolygon(mShape).category(Short.parseShort("1111111111111111")).mask((short)0));
+        physicsBody.createFixture(fixturedef.shapePolygon(mShape).category((short)-1).mask((short)0));
+        
         setPhysicsBody(physicsBody);
     }
 
