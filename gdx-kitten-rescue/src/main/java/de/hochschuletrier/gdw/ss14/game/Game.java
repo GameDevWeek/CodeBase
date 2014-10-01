e0e9ce76b4ba6601ec347ff5c0c4fc2dc7b69160
Djitin <djtinituz@googlemail.com>
2014-10-01 15:47:47 +0200

    MapManager loads and renders the map now.

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/game/Game.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/game/Game.java
@@ -1,14 +1,18 @@
 package de.hochschuletrier.gdw.ss14.game;
 
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.math.*;
 import com.badlogic.gdx.utils.*;
+
 import de.hochschuletrier.gdw.commons.gdx.assets.*;
 import de.hochschuletrier.gdw.commons.gdx.physix.*;
 import de.hochschuletrier.gdw.commons.tiled.*;
 import de.hochschuletrier.gdw.ss14.ecs.*;
 import de.hochschuletrier.gdw.ss14.ecs.components.*;
 import de.hochschuletrier.gdw.ss14.ecs.systems.*;
+
 import org.slf4j.Logger;
 import org.slf4j.*;
 
@@ -46,9 +50,11 @@ public class Game
 
     public void init(AssetManagerX assetManager)
     {
-
         initializeSystems();
         initializeTestComponents();
+        
+        mapManager.loadMap("ErsteTestMap");        
+        mapManager.setFloor(0);
     }
 
     private void initializeSystems()
@@ -69,39 +75,7 @@ public class Game
 
     private void initializeTestComponents()
     {
-
-        // Level entity        
-        TileMapRenderingComponent newTmrComp = new TileMapRenderingComponent();
-        newTmrComp.map = loadMap("data/maps/ErsteTestMap.tmx");
-        newTmrComp.renderedLayers.add(0);
-        newTmrComp.renderedLayers.add(1);
-
-        // Render everything of floor 0
-        int currentFloor = 0;
-
-        for (Layer layer : newTmrComp.map.getLayers())
-        {
-            //if (layer.getIntProperty("floor", -1) == currentFloor)
-            newTmrComp.renderedLayers.add(newTmrComp.map.getLayers().indexOf(layer));
-        }
-
-        int levelEntity = entityManager.createEntity();
-        entityManager.addComponent(levelEntity, newTmrComp);
         catEntity = EntityFactory.constructCat(mapCenter.cpy(), 10.0f, 5.0f, 0.1f, 0.1f);
-
-        // Cat entity        
-        /*CameraComponent newCamComp = new CameraComponent();
-        newCamComp.cameraZoom = 1.0f;
-        
-        PhysicsComponent newPhysicsComp = new PhysicsComponent();
-        mapCenter = new Vector2(newTmrComp.map.getWidth()*newTmrComp.map.getTileWidth() * 0.5f, 
-                                newTmrComp.map.getHeight()*newTmrComp.map.getTileHeight() * 0.5f);
-        newPhysicsComp.dummyPosition = mapCenter.cpy();
-        
-        catEntity = entityManager.createEntity();
-        entityManager.addComponent(catEntity, newCamComp);
-        entityManager.addComponent(catEntity, newPhysicsComp);*/
-
     }
 
     public void addSystem(ECSystem system)
