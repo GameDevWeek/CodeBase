50ac20099c11400dc8a0cf8cbb7e1f203b248abf
Djitin <djtinituz@googlemail.com>
2014-10-01 11:37:31 +0200

    Moved the engine code from Game.java to Engine

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/game/Game.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/game/Game.java
@@ -1,36 +1,25 @@
 package de.hochschuletrier.gdw.ss14.game;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Array;
 
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.commons.tiled.LayerObject;
 import de.hochschuletrier.gdw.commons.tiled.TiledMap;
+import de.hochschuletrier.gdw.ss14.ecs.Engine;
 import de.hochschuletrier.gdw.ss14.ecs.EntityManager;
 import de.hochschuletrier.gdw.ss14.ecs.components.TileMapRenderingComponent;
 import de.hochschuletrier.gdw.ss14.ecs.systems.CameraSystem;
-import de.hochschuletrier.gdw.ss14.ecs.systems.ECSystem;
 import de.hochschuletrier.gdw.ss14.ecs.systems.InputSystem;
 import de.hochschuletrier.gdw.ss14.ecs.systems.MovementSystem;
 import de.hochschuletrier.gdw.ss14.ecs.systems.TileMapRenderingSystem;
 import de.hochschuletrier.gdw.ss14.ecs.components.CameraComponent;
 import de.hochschuletrier.gdw.ss14.ecs.components.PhysicsComponent;
 
-import java.util.Comparator;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Game {
 
-    private static final Logger logger = LoggerFactory.getLogger(Game.class);
-
-    private static SystemComparator comparator = new SystemComparator();
-    
-    private Array<ECSystem> systems;
+    private Engine engine = null;   
     private EntityManager entityManager;
     
     private int catEntity;
@@ -39,10 +28,11 @@ public class Game {
     public Game() {
         
         entityManager = new EntityManager();
-        systems = new Array<ECSystem>();
     }
 
     public void init(AssetManagerX assetManager) {
+        
+        engine = new Engine();
         
         initializeSystems();        
         initializeTestComponents();
@@ -51,12 +41,12 @@ public class Game {
     private void initializeSystems() {
         
         // Game logic related systems
-        addSystem( new InputSystem(entityManager) );
-        addSystem( new MovementSystem(entityManager) );
-        addSystem( new CameraSystem(entityManager, 1024) );
+        engine.addSystem( new InputSystem(entityManager) );
+        engine.addSystem( new MovementSystem(entityManager) );
+        engine.addSystem( new CameraSystem(entityManager, 1024) );
         
         // Rendering related systems
-        addSystem( new TileMapRenderingSystem(entityManager, 0) );        
+        engine.addSystem( new TileMapRenderingSystem(entityManager, 0) );        
     }
     
     private void initializeTestComponents() {
@@ -85,18 +75,6 @@ public class Game {
         
     }
     
-    public void addSystem(ECSystem system) {
-        
-        systems.add(system);
-        systems.sort(comparator);
-    }
-    
-    public void removeSystem(ECSystem system) {
-        
-        systems.removeValue(system, true);
-    }
-
-
     public TiledMap loadMap(String filename) {
         
         try {
@@ -124,36 +102,11 @@ public class Game {
         else
             catPhysicsComp.dummyPosition.add(mapCenter.cpy().sub(catPhysicsComp.getPosition()));
         
-        for (ECSystem system : systems) {
-            system.update(delta);
-        }
+        engine.update(delta);
     }
 
     public void render() {
         
-        for (ECSystem system : systems) {
-            system.render();
-        }
-    }
-
-    private static class SystemComparator implements Comparator<ECSystem>
-    {
-
-        @Override
-        public int compare(ECSystem a, ECSystem b) {
-            int result;
-
-            if (a.getPriority() > b.getPriority()) {
-                result = 1;
-            }
-            else if (a.getPriority() == b.getPriority()) {
-                result = 0;
-            }
-            else {
-                result = -1;
-            }
-
-            return result;
-        }
+        engine.render();
     }
 }
