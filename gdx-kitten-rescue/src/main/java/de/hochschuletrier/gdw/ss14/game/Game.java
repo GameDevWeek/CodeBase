22eeb6363832049369575b61872b609d5f421ff5
DerjenigeWelche <masterfg@gmx.net>
2014-10-01 16:33:40 +0200

    Test

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/game/Game.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/game/Game.java
@@ -84,7 +84,7 @@ public class Game
 
     private void initializeTestComponents()
     {
-        catEntity = EntityFactory.constructCat(mapCenter.cpy(), 10.0f, 5.0f, 0.1f, 0.1f);
+        catEntity = EntityFactory.constructCat(new Vector2(300,300), 150.0f, 75.0f, 0, 100);
     }
 
     public void addSystem(ECSystem system)
