51ae75c1026689bee2a489da6fc08cc84c593540
Rhabarberkuchen <michaela.moes@gmx.net>
2014-10-01 14:11:43 +0200

    Cat- und DogStateComponent hinzugefügt

---
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/components/CatStateComponent.java
@@ -0,0 +1,14 @@
+package de.hochschuletrier.gdw.ss14.ecs.components;
+
+public class CatStateComponent implements Component{
+
+    public enum CatStateEnum {
+        WALKING,
+        JUMPING,
+        RUNNING,
+        STANDING,
+        DISTRACTED
+    }
+
+
+}
