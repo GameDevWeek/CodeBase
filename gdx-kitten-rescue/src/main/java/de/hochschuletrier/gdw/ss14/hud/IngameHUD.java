261d251268ee967ea23cf72cc513c714fbc8a684
Sonic <Sonic@home.de>
2014-10-02 17:15:08 +0200

    HUD Implementation improved

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/hud/IngameHUD.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/hud/IngameHUD.java
@@ -1,21 +1,33 @@
 package de.hochschuletrier.gdw.ss14.hud;
 
+import com.badlogic.gdx.Gdx;
+
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 
 public class IngameHUD {
-	private CatLiveHUD catLive;
+	private CatLifeHUD catLife;
 	private WaterlevelHUD waterlevel;
 	
 	public IngameHUD(AssetManagerX assetManager) {
-		catLive = new CatLiveHUD(assetManager);
-		waterlevel = new WaterlevelHUD(assetManager, catLive);
+		catLife = new CatLifeHUD(assetManager);
+		catLife.setScale(0.2f);
+		
+		waterlevel = new WaterlevelHUD(assetManager, 25f, catLife.getWidth());
 	}
     public void render() {
-    	catLive.render();
+        catLife.setPosition(Gdx.graphics.getWidth() - catLife.getWidth() - 10, 20);
+        waterlevel.setPosition(catLife.getX(), catLife.getY() + catLife.getHeigth());
+        
+    	catLife.render();
     	waterlevel.render();
     }
     
     public void update() {
-        
+        /*
+        EntityManager man = new EntityManager();
+        int playerid = man.getAllEntitiesWithComponents(PlayerComponent.class, CatPropertyComponent.class).first();
+        CatPropertyComponent cat = man.getComponent(playerid, CatPropertyComponent.class);
+        cat.amountLives
+        */
     }
 }
