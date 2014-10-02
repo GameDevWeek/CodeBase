4069477fb35f3c641a77fe9ab5749d8de06ec6bd
Benjamin 'Albsi' Albsmeier <albsmeier@gmail.com>
2014-10-02 12:28:13 +0200

    movement stuff - sliding

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/components/MovementComponent.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ecs/components/MovementComponent.java
@@ -13,6 +13,7 @@ public class MovementComponent implements Component{
 	public final float MIN_VELOCITY;
 	
 	public Vector2 directionVec;
+    public Vector2 lastDirectionVec;
 	
 	public MovementComponent(float maxVelo, float middleVelo, float minVelo, float acceleration){
 		MAX_VELOCITY = maxVelo;
