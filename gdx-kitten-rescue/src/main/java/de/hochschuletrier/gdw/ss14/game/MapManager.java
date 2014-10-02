4173420a67ad1bbe86fccd5cc03fa5ec437bf741
Djitin <djtinituz@googlemail.com>
2014-10-02 13:28:38 +0200

    Renderer doesn't render object layer anymore.

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/game/MapManager.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/game/MapManager.java
@@ -94,8 +94,9 @@ public class MapManager
         for (Layer layer : mapComp.getMap().getLayers())
         {
             System.out.println(this.getClass().getName()+": "+layer.getIntProperty("floor", -1));
-            //if (layer.getIntProperty("floor", -1) == floor)
-            mapComp.renderedLayers.add(mapComp.getMap().getLayers().indexOf(layer));
+            //if (layer.getIntProperty("floor", -1) == floor) 
+            if (layer.isTileLayer())
+                mapComp.renderedLayers.add(mapComp.getMap().getLayers().indexOf(layer));
         }
     }
 
