ee64095ebbdb23ed2a95ff3fc1db38109838f20a
Sonic <Sonic@home.de>
2014-10-02 17:46:53 +0200

    WeaponHUD implemented

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/hud/IngameHUD.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/hud/IngameHUD.java
@@ -7,19 +7,25 @@ import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 public class IngameHUD {
 	private CatLifeHUD catLife;
 	private WaterlevelHUD waterlevel;
+	private WeaponHUD weapon;
 	
 	public IngameHUD(AssetManagerX assetManager) {
 		catLife = new CatLifeHUD(assetManager);
 		catLife.setScale(0.2f);
 		
 		waterlevel = new WaterlevelHUD(assetManager, 25f, catLife.getWidth());
+		
+		weapon = new WeaponHUD(assetManager);
+		weapon.setScale(0.7f);
 	}
     public void render() {
-        catLife.setPosition(Gdx.graphics.getWidth() - catLife.getWidth() - 10, 20);
+        weapon.setPosition(Gdx.graphics.getWidth() - weapon.getWidth() - 15, 25);
+        catLife.setPosition(weapon.getX() - catLife.getWidth() - 10, weapon.getY());
         waterlevel.setPosition(catLife.getX(), catLife.getY() + catLife.getHeigth());
         
     	catLife.render();
     	waterlevel.render();
+    	weapon.render();
     }
     
     public void update() {
