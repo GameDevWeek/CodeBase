7d63b7096ce41d6214e28c9a13e7d79f66921eab
Schirace <tm@christianmeier.de>
2014-10-02 14:05:52 +0200

    katzenleben

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ui/ingame/CatLiveUI.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/ui/ingame/CatLiveUI.java
@@ -13,22 +13,39 @@ public class CatLiveUI {
 private AssetManagerX assetManager;
 	
 	Texture catLiveFull;
+	Texture catLiveEmpty;
+	
+	int maxLeben = 9;
+	int aktulleLeben = 3;
+	
+	float scale = 0.2f;
 	
 	public CatLiveUI(AssetManagerX assetManager) {
 		this.assetManager = assetManager;
 		
 		catLiveFull = assetManager.getTexture("cat_live_full");
+		catLiveEmpty = assetManager.getTexture("cat_live_empty");
 	}
 	
 	public void render() {
 		Main.getInstance().screenCamera.bind();
         
-        DrawUtil.batch.draw(catLiveFull, 0, 0, catLiveFull.getWidth(), catLiveFull.getHeight(), 0, 0,
-        		catLiveFull.getWidth(), catLiveFull.getHeight(), false, true);
-        /*
-        DrawUtil.batch.draw(walking.getKeyFrame(stateTime), x,
-                512, walking.getKeyFrame(0f).getRegionWidth(), -walking.getKeyFrame(0f).getRegionHeight());
-                */
+		for( int i = 1; i <= aktulleLeben; i++) {
+			DrawUtil.batch.draw(catLiveFull, 
+					Gdx.graphics.getWidth() - ((catLiveFull.getWidth() * scale) * i),
+					20, 
+					catLiveFull.getWidth() * scale,
+					catLiveFull.getHeight() * scale,
+					0, 0, catLiveFull.getWidth(), catLiveFull.getHeight(), false, true);
+		}
+		for( int i = aktulleLeben + 1; i <= maxLeben; i++) {
+			DrawUtil.batch.draw(catLiveEmpty, 
+					Gdx.graphics.getWidth() - ((catLiveEmpty.getWidth() * scale) * i),
+					20, 
+					catLiveEmpty.getWidth()* scale,
+					catLiveEmpty.getHeight() *scale,
+					0, 0, catLiveEmpty.getWidth(), catLiveEmpty.getHeight(), false, true);
+		}
 	}
 
 }
