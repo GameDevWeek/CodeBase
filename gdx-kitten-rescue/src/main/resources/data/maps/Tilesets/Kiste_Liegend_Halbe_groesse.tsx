ceb5fd92293c3ee6653febd30d7694ba0407095e
Shentoza <wuelk@hotmail.de>
2014-10-02 18:13:15 +0200

    adjusted maps for gap fix. removed box(sideways).

--- gdx-kitten-rescue/src/main/resources/data/maps/Tilesets/Kiste_Liegend_Halbe_groesse.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Kiste_Liegend_Halbe_groesse" tilewidth="192" tileheight="128">
- <tileoffset x="1" y="1"/>
- <image source="Kiste_Liegend_Halbe_groesse.png" width="384" height="128"/>
-</tileset>
