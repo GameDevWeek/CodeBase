28c7858d938eb089795833aa293ef9b2e78bc565
MikO <miko007@me.com>
2014-10-03 14:23:40 +0200

    changed soundsystem to catsoundlistener

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/states/GameplayState.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/states/GameplayState.java
@@ -21,6 +21,7 @@ import de.hochschuletrier.gdw.ss14.Main;
 import de.hochschuletrier.gdw.ss14.game.Game;
 import de.hochschuletrier.gdw.ss14.hud.IngameHUD;
 import de.hochschuletrier.gdw.ss14.physics.PhysicsActions;
+import de.hochschuletrier.gdw.ss14.sound.CatSoundListener;
 import de.hochschuletrier.gdw.ss14.sound.LocalMusic;
 import de.hochschuletrier.gdw.ss14.sound.SoundManager;
 
@@ -35,9 +36,10 @@ public class GameplayState extends GameState implements InputProcessor {
     private Sound helicopter;
     private final Vector2 cursor = new Vector2();
     private final FpsCalculator fpsCalc = new FpsCalculator(200, 100, 16);
-    private LocalMusic music;
+    private LocalMusic music, hurryMusic;
 
     private final SoundEmitter emitter = new SoundEmitter();
+	private static CatSoundListener playerSoundListener;
     private final LimitedSmoothCamera camera = new LimitedSmoothCamera();
     private final Vector2 position = new Vector2(100, 100);
     private float totalMapWidth, totalMapHeight;
@@ -54,6 +56,7 @@ public class GameplayState extends GameState implements InputProcessor {
         game = new Game(assetManager);
         game.init(assetManager);
         this.music = Main.MusicManager.getMusicStreamByStateName(GameStates.GAMEPLAY);
+        this.playerSoundListener = new CatSoundListener(assetManager);
         Main.inputMultiplexer.addProcessor(this);
         
         ingameHUD = new IngameHUD(assetManager);
@@ -87,7 +90,7 @@ public class GameplayState extends GameState implements InputProcessor {
     @Override
     public void update(float delta) {
     	this.music.update();
-    	SoundManager.performAction(PhysicsActions.CATWALK);
+    	this.playerSoundListener.updateCat();
         /*emitter.update();
         emitter.setPosition(cursor.x, cursor.y, 0);
         game.update(delta);
