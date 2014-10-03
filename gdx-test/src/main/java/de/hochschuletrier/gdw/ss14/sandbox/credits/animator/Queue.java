43a03a1bbf113578542b52b041f6f85b840cd55b
Lusito <core@roughael.net>
2014-10-03 16:01:04 +0200

    credits weiter gemacht

---
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/credits/animator/Queue.java
@@ -0,0 +1,12 @@
+package de.hochschuletrier.gdw.ss14.sandbox.credits.animator;
+
+import java.util.ArrayList;
+
+public class Queue {
+
+    public Path path;
+    public int time;
+    public Queue next;
+    public Queue finalNext;
+    public ArrayList<Item> items;
+}
