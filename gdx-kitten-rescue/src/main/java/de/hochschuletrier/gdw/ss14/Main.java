355f7e6b643593c63fec5c356020d65074154f1b
Markus Schmieder <sellget@gmx.de>
2014-10-05 13:14:09 +0200

    Change entrypoint to MAINMENU

--- gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/Main.java
+++ gdx-kitten-rescue/src/main/java/de/hochschuletrier/gdw/ss14/Main.java
@@ -155,7 +155,7 @@ public class Main extends StateBasedGame<KittenGameState> {
             }
         }
 
-        GameStateEnum.STARTSCREEN.activate();
+        GameStateEnum.MAINMENU.activate();							// This is the entrypoint
     }
 
     @Override
