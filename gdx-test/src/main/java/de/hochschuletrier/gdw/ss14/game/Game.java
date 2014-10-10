8595a82caecc5ae399ddfe1cfda0d5cd440534c5
Lusito <core@roughael.net>
2014-10-11 01:42:53 +0200

    AnimationRenderSystem

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/game/Game.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/game/Game.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import de.hochschuletrier.gdw.commons.gdx.assets.AnimationExtended;
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixBodyDef;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixComponentAwareContactListener;
@@ -16,9 +17,12 @@ import de.hochschuletrier.gdw.commons.gdx.physix.components.PhysixModifierCompon
 import de.hochschuletrier.gdw.commons.gdx.physix.systems.PhysixDebugRenderSystem;
 import de.hochschuletrier.gdw.commons.gdx.physix.systems.PhysixSystem;
 import de.hochschuletrier.gdw.ss14.Main;
+import de.hochschuletrier.gdw.ss14.game.components.AnimationComponent;
 import de.hochschuletrier.gdw.ss14.game.components.ImpactSoundComponent;
+import de.hochschuletrier.gdw.ss14.game.components.PositionComponent;
 import de.hochschuletrier.gdw.ss14.game.contactlisteners.ImpactSoundListener;
 import de.hochschuletrier.gdw.ss14.game.systems.AnimationRenderSystem;
+import de.hochschuletrier.gdw.ss14.game.systems.UpdatePositionSystem;
 import de.hochschuletrier.gdw.ss14.game.utils.PhysixUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,18 +52,22 @@ public class Game extends InputAdapter {
     private final PhysixSystem physixSystem = new PhysixSystem(BOX2D_SCALE, STEP_SIZE, VELOCITY_ITERATIONS, POSITION_ITERATIONS, PRIORITY_PHYSIX);
     private final PhysixDebugRenderSystem physixDebugRenderSystem = new PhysixDebugRenderSystem(physixSystem.getWorld(), physixSystem.getScale(), PRIORITY_DEBUG_WORLD);
     private final AnimationRenderSystem animationRenderSystem = new AnimationRenderSystem(PRIORITY_ANIMATIONS);
+    private final UpdatePositionSystem updatePositionSystem = new UpdatePositionSystem(PRIORITY_PHYSIX+1);
     
     private Sound impactSound;
+    private AnimationExtended ballAnimation;
 
     public Game() {
     }
 
     public void init(AssetManagerX assetManager) {
         impactSound = assetManager.getSound("click");
+        ballAnimation = assetManager.getAnimation("ball");
         
         engine.addSystem(physixSystem);
         engine.addSystem(physixDebugRenderSystem);
         engine.addSystem(animationRenderSystem);
+        engine.addSystem(updatePositionSystem);
         PhysixComponentAwareContactListener contactListener = new PhysixComponentAwareContactListener();
         physixSystem.getWorld().setContactListener(contactListener);
         contactListener.addListener(ImpactSoundComponent.class, new ImpactSoundListener());
@@ -83,21 +91,27 @@ public class Game extends InputAdapter {
 
     public void createBall(float x, float y, float radius) {
         Entity entity = engine.createEntity();
+        entity.add(engine.createComponent(PositionComponent.class));
         PhysixModifierComponent modifyComponent = new PhysixModifierComponent();
         entity.add(modifyComponent);
 
         ImpactSoundComponent soundComponent = engine.createComponent(ImpactSoundComponent.class);
         soundComponent.init(impactSound, 20, 20, 100);
         entity.add(soundComponent);
+        
+        AnimationComponent animComponent = engine.createComponent(AnimationComponent.class);
+        animComponent.animation = ballAnimation;
+        entity.add(animComponent);
 
         modifyComponent.schedule(() -> {
-            PhysixBodyComponent component = engine.createComponent(PhysixBodyComponent.class);
+            PhysixBodyComponent bodyComponent = engine.createComponent(PhysixBodyComponent.class);
             PhysixBodyDef bodyDef = new PhysixBodyDef(BodyType.DynamicBody, physixSystem)
                     .position(x, y).fixedRotation(false);
-            component.init(bodyDef, physixSystem, entity);
+            bodyComponent.init(bodyDef, physixSystem, entity);
             PhysixFixtureDef fixtureDef = new PhysixFixtureDef(physixSystem)
                     .density(5).friction(0.2f).restitution(0.4f).shapeCircle(radius);
-            component.createFixture(fixtureDef);
+            bodyComponent.createFixture(fixtureDef);
+            entity.add(bodyComponent);
         });
         engine.addEntity(entity);
     }
