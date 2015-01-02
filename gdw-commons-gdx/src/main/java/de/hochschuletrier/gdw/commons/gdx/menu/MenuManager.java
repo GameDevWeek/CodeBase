9f0f5e2061793865f38aa11ff19fde4ee41c575e
Lusito <core@roughael.net>
2015-01-02 03:47:50 +0100

    Some helper classes to make creating menus easier

---
+++ gdw-commons-gdx/src/main/java/de/hochschuletrier/gdw/commons/gdx/menu/MenuManager.java
@@ -0,0 +1,131 @@
+package de.hochschuletrier.gdw.commons.gdx.menu;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import de.hochschuletrier.gdw.commons.gdx.input.InputInterceptor;
+import de.hochschuletrier.gdw.commons.gdx.state.ScreenListener;
+import de.hochschuletrier.gdw.commons.gdx.utils.DrawUtil;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Santo Pfingsten
+ */
+public class MenuManager implements ScreenListener, Disposable {
+
+    private final Stage stage = new Stage(new ScreenViewport(), DrawUtil.batch);
+    private final Table table;
+    private final Stack stack;
+    private final int width;
+    private final int height;
+    private Actor currentPage;
+    private LinkedList<Actor> pageStack = new LinkedList();
+    private final InputInterceptor inputProcessor;
+
+    public MenuManager(int width, int height) {
+        this.width = width;
+        this.height = height;
+        stack = new Stack();
+        table = new Table();
+        table.setFillParent(true);
+        table.setTransform(false);
+        table.row().expand().fill();
+        table.add().colspan(3).expand().fill();
+        table.row();
+        table.add().expandX().fillX();
+        table.add(stack).minSize(width, height).maxSize(width, height);
+        table.add().expandX().fillX();
+        table.row().expand().fill();
+        table.add().colspan(3).expand().fill();
+        stage.addActor(table);
+
+        inputProcessor = new InputInterceptor(stage) {
+
+            @Override
+            public boolean keyUp(int keycode) {
+                if (isActive && keycode == Input.Keys.ESCAPE) {
+                    popPage();
+                    return true;
+                }
+                return super.keyUp(keycode);
+            }
+        };
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public InputProcessor getInputProcessor() {
+        return inputProcessor;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    @Override
+    public void resize(int screenWidth, int screenHeight) {
+        stage.getViewport().update(screenWidth, screenHeight, true);
+    }
+
+    public void addLayer(Actor actor) {
+        actor.setBounds(0, 0, width, height);
+        stack.addActor(actor);
+    }
+
+    public void update(float delta) {
+        stage.act(delta);
+    }
+
+    public void render() {
+        DrawUtil.batch.end();
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.draw();
+        DrawUtil.batch.begin();
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+
+    public void pushPage(Actor page) {
+        if (currentPage != null) {
+            currentPage.setVisible(false);
+            pageStack.push(currentPage);
+        }
+        currentPage = page;
+        currentPage.setVisible(true);
+    }
+
+    public void popPage() {
+        stage.unfocusAll();
+        Gdx.input.setOnscreenKeyboardVisible(false);
+
+        if (pageStack.isEmpty()) {
+            // todo
+        } else {
+            currentPage.setVisible(false);
+            currentPage = pageStack.pop();
+            currentPage.setVisible(true);
+        }
+    }
+
+    public void enableInput(boolean enabled) {
+        inputProcessor.setActive(enabled);
+        inputProcessor.setBlocking(enabled);
+    }
+}
