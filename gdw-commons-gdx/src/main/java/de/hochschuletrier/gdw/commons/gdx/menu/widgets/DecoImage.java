ca78ef3e048dce8a53334a2fb68bd5051635aaf2
Lusito <core@roughael.net>
2015-01-03 21:43:28 +0100

    allow for change of texture

--- gdw-commons-gdx/src/main/java/de/hochschuletrier/gdw/commons/gdx/menu/widgets/DecoImage.java
+++ gdw-commons-gdx/src/main/java/de/hochschuletrier/gdw/commons/gdx/menu/widgets/DecoImage.java
@@ -1,19 +1,32 @@
 package de.hochschuletrier.gdw.commons.gdx.menu.widgets;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Scaling;
 
 /**
  *
  * @author Santo Pfingsten
  */
 public class DecoImage extends Image {
+    TextureRegion region;
 
     public DecoImage(Texture texture) {
-        super(texture);
+        super(null, Scaling.stretch, Align.center);
+        region = new TextureRegion(texture);
+        setDrawable(new TextureRegionDrawable(region));
+		setSize(getPrefWidth(), getPrefHeight());
+        
         setBounds(0, 0, texture.getWidth(), texture.getHeight());
         setOrigin(texture.getWidth() / 2, texture.getHeight() / 2);
         setTouchable(Touchable.disabled);
+    }
+    
+    public void setTexture(Texture texture) {
+        region.setRegion(texture);
     }
 }
