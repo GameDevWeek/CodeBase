f48bb147c4b25fef9e8d0a126cb19eaf4ef5bb61
Lusito <core@roughael.net>
2015-01-06 01:04:59 +0100

    enabling vsync

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/Main.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/Main.java
@@ -182,7 +182,7 @@ public class Main extends StateBasedGame {
         cfg.width = WINDOW_WIDTH;
         cfg.height = WINDOW_HEIGHT;
         cfg.useGL30 = false;
-        cfg.vSyncEnabled = false;
+        cfg.vSyncEnabled = true;
         cfg.foregroundFPS = 60;
         cfg.backgroundFPS = 60;
 
