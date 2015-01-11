92375819d19d7472a1e8e3dbd08f80c551110231
Lusito <core@roughael.net>
2015-01-11 04:21:27 +0100

    Fixed timestep fix and extra info

--- gdw-commons-gdx/src/main/java/de/hochschuletrier/gdw/commons/gdx/physix/systems/PhysixSystemFixedStep.java
+++ gdw-commons-gdx/src/main/java/de/hochschuletrier/gdw/commons/gdx/physix/systems/PhysixSystemFixedStep.java
@@ -4,7 +4,9 @@ import com.badlogic.ashley.core.Entity;
 
 /**
  * A physics system with a fixed time step. Online tutorials will tell you to use a fixed step to avoid problems.
- * In my testcases, it always caused stuttering, so I made this optional. Use if you encounter simulation problems with PhysixSystem.
+ * In my test cases, it always caused stuttering, so I made this optional. Use if you encounter simulation problems with PhysixSystem.
+ * You can reduce the stuttering by using a higher framerate for the physics than the rendering.
+ * For example, if you render at 60 FPS, use a framerate of 120 or even 240 for physics.
  *
  * @author Santo Pfingsten
  */
