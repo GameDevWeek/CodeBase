f36e50bd9f540286c8b598279aea8fe224bddcdb
Lusito <core@roughael.net>
2015-03-08 13:48:09 +0100

    Optimizing Demo for smaller screen

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/game/Game.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/game/Game.java
@@ -93,12 +93,12 @@ public class Game extends InputAdapter {
 
     private void setupPhysixWorld() {
         physixSystem.setGravity(0, 24);
-        PhysixBodyDef bodyDef = new PhysixBodyDef(BodyDef.BodyType.StaticBody, physixSystem).position(410, 800).fixedRotation(false);
+        PhysixBodyDef bodyDef = new PhysixBodyDef(BodyDef.BodyType.StaticBody, physixSystem).position(410, 500).fixedRotation(false);
         Body body = physixSystem.getWorld().createBody(bodyDef);
         body.createFixture(new PhysixFixtureDef(physixSystem).density(1).friction(0.5f).shapeBox(800, 20));
         PhysixUtil.createHollowCircle(physixSystem, 180, 180, 150, 30, 6);
 
-        createTrigger(410, 1000, 1600, 40, (Entity entity) -> {
+        createTrigger(410, 600, 3200, 40, (Entity entity) -> {
             engine.removeEntity(entity);
         });
     }
