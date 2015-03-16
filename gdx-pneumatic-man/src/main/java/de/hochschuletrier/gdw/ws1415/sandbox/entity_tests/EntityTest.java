7da1620a84238cc21117eace17fdcf509f6ccaf4
Hati Eth <hati.eth@gmail.com>
2015-03-16 14:11:27 +0100

    added imports

---
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/sandbox/entity_tests/EntityTest.java
@@ -0,0 +1,26 @@
+package de.hochschuletrier.gdw.ws1415.sandbox.entity_tests;
+
+import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
+import de.hochschuletrier.gdw.ws1415.sandbox.SandboxGame;
+
+public class EntityTest extends SandboxGame {
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(AssetManagerX assetManager) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
