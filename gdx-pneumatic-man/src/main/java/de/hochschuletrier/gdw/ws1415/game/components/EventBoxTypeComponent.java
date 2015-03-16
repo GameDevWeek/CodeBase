09440c47f5c7fa4eb428cf9a4afcfcc450bdd98b
Aggrobatis <sellget@gmx.de>
2015-03-16 16:06:48 +0100

    + EntityCreator - class (static) + EventBoxTypeComponent

---
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/components/EventBoxTypeComponent.java
@@ -0,0 +1,10 @@
+package de.hochschuletrier.gdw.ws1415.game.components;
+
+import com.badlogic.ashley.core.Component;
+
+import de.hochschuletrier.gdw.ws1415.game.utils.EventBoxType;
+
+public class EventBoxTypeComponent extends Component
+{
+    EventBoxType type;
+}
