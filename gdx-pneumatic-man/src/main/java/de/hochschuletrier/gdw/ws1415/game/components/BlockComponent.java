5e021f070c8cbcabcbe6e29ec0d927356700b75a
compix <wladimirkroeker@googlemail.com>
2015-03-16 18:22:34 +0100

    no message

---
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/components/BlockComponent.java
@@ -0,0 +1,12 @@
+package de.hochschuletrier.gdw.ws1415.game.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool;
+
+public class BlockComponent extends Component implements Pool.Poolable {
+
+	@Override
+	public void reset() {
+	}
+
+}
