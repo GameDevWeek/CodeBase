2df32a249977e7c8888146f060508b62d26e0abe
Hati Eth <hati.eth@gmail.com>
2015-03-16 13:47:52 +0100

    EntityTest SandboEntityTest Sandbox

---
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/entity_tests/EntityTest.java
@@ -0,0 +1,26 @@
+package de.hochschuletrier.gdw.ss14.sandbox.entity_tests;
+
+import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
+import de.hochschuletrier.gdw.ss14.sandbox.SandboxGame;
+
+public class EntityTest extends SandboxGame {
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(AssetManagerX assetManager) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
