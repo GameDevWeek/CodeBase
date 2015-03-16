43d460294e616f0226142e618fe5b77101912084
oliver <biwero@uni-trier.de>
2015-03-16 16:57:11 +0100

    added important components added player contact listener

---
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/components/PlayerComponent.java
@@ -0,0 +1,10 @@
+package de.hochschuletrier.gdw.ws1415.game.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * Created by oliver on 16.03.15.
+ */
+public class PlayerComponent extends Component {
+
+}
